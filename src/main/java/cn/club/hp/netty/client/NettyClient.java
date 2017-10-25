42321e1c235bbc0e0a61e36558e49915f55703c2
'club-hp' <hpd84_0321@163.com>
2017-10-26 02:18:48 +0800

    想啊爱爱

--- src/main/java/cn/club/hp/netty/client/NettyClient.java
+++ src/main/java/cn/club/hp/netty/client/NettyClient.java
@@ -15,6 +15,7 @@ public class NettyClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println(" Netty Client");
+     System.out.println("新增功能再河北");
 	}
 
 }
