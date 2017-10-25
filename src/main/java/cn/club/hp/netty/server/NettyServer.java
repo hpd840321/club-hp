22a67aa292b3f54e722fd87bd1ded36e08e8d1a6
'club-hp' <hpd84_0321@163.com>
2017-10-26 02:03:54 +0800

    新增netty服务端功能

---
+++ src/main/java/cn/club/hp/netty/server/NettyServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cn.club.hp.netty.server;
+
+/**
+ * @author 黄平
+ *
+ */
+public class NettyServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Netty Server");
+	}
+
+}
