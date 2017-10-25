205222a81e970d0354746da7e3e2390523dbf4be
'club-hp' <hpd84_0321@163.com>
2017-10-26 02:08:47 +0800

    新增Netty客户端功能

---
+++ src/main/java/cn/club/hp/netty/client/NettyClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cn.club.hp.netty.client;
+
+/**
+ * @author 黄平
+ *
+ */
+public class NettyClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println(" Netty Client");
+	}
+
+}
