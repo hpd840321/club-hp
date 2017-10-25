4600a36fdd9625761857466c70a81039c6b87143
'club-hp' <hpd84_0321@163.com>
2017-10-26 01:57:29 +0800

    新增功能

---
+++ src/main/java/cn/club/hp/netty/NettyTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cn.club.hp.netty;
+
+/**
+ * @author 黄平
+ *
+ */
+public class NettyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("test");
+	}
+
+}
